5d808ae1c2d8fc4a0f0afbfc906057068669fa65
Miriam Suzanne <miriam@oddbird.net>
2022-11-27 12:48:00 -0700

    Build twitter archive

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,7 +1,7 @@
 let data = {
-	username: "eleven_ty", // No leading @ here
-	homeLabel: "11ty.dev",
-	homeUrl: "https://www.11ty.dev/",
+	username: "terribleMia", // No leading @ here
+	homeLabel: "miriamsuzanne.com",
+	homeUrl: "https://www.miriamsuzanne.com/",
 };
 
 data.avatar = `https://v1.indieweb-avatar.11ty.dev/${encodeURIComponent(data.homeUrl)}/`;
