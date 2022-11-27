fa1809f4d1130111ac27ba7ca99d7566b5c812ce
Miriam Suzanne <miriam@oddbird.net>
2022-11-27 12:48:00 -0700

    Build twitter archive

---
+++ database/twitter-circle-tweet.js
@@ -0,0 +1,207 @@
+module.exports = [
+  {
+    "tweet" : {
+      "retweeted" : false,
+      "source" : "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+      "entities" : {
+        "hashtags" : [ ],
+        "symbols" : [ ],
+        "user_mentions" : [
+          {
+            "name" : "Hallojeena 🧛🏻‍♀️",
+            "screen_name" : "jina",
+            "indices" : [
+              "0",
+              "5"
+            ],
+            "id_str" : "8501792",
+            "id" : "8501792"
+          }
+        ],
+        "urls" : [ ]
+      },
+      "display_text_range" : [
+        "0",
+        "35"
+      ],
+      "favorite_count" : "1",
+      "in_reply_to_status_id_str" : "1558243572242976768",
+      "id_str" : "1558249388928970752",
+      "in_reply_to_user_id" : "8501792",
+      "truncated" : false,
+      "retweet_count" : "0",
+      "id" : "1558249388928970752",
+      "in_reply_to_status_id" : "1558243572242976768",
+      "created_at" : "Sat Aug 13 00:29:39 +0000 2022",
+      "favorited" : false,
+      "full_text" : "@jina ugh, I'm sorry, that's awful.",
+      "lang" : "en",
+      "in_reply_to_screen_name" : "jina",
+      "in_reply_to_user_id_str" : "8501792"
+    }
+  },
+  {
+    "tweet" : {
+      "retweeted" : false,
+      "source" : "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+      "entities" : {
+        "hashtags" : [ ],
+        "symbols" : [ ],
+        "user_mentions" : [
+          {
+            "name" : "CSS Evils 😈 !important",
+            "screen_name" : "cassiecodes",
+            "indices" : [
+              "0",
+              "12"
+            ],
+            "id_str" : "758598429115310080",
+            "id" : "758598429115310080"
+          }
+        ],
+        "urls" : [ ]
+      },
+      "display_text_range" : [
+        "0",
+        "74"
+      ],
+      "favorite_count" : "1",
+      "in_reply_to_status_id_str" : "1580400776744083458",
+      "id_str" : "1580401331050717185",
+      "in_reply_to_user_id" : "758598429115310080",
+      "truncated" : false,
+      "retweet_count" : "0",
+      "id" : "1580401331050717185",
+      "in_reply_to_status_id" : "1580400776744083458",
+      "created_at" : "Thu Oct 13 03:33:34 +0000 2022",
+      "favorited" : false,
+      "full_text" : "@cassiecodes I'm sorry, and I promise to be more present in the future. :)",
+      "lang" : "en",
+      "in_reply_to_screen_name" : "cassiecodes",
+      "in_reply_to_user_id_str" : "758598429115310080"
+    }
+  },
+  {
+    "tweet" : {
+      "retweeted" : false,
+      "source" : "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+      "entities" : {
+        "hashtags" : [ ],
+        "symbols" : [ ],
+        "user_mentions" : [
+          {
+            "name" : "Cassie Evans",
+            "screen_name" : "cassiecodes",
+            "indices" : [
+              "0",
+              "12"
+            ],
+            "id_str" : "758598429115310080",
+            "id" : "758598429115310080"
+          }
+        ],
+        "urls" : [ ]
+      },
+      "display_text_range" : [
+        "0",
+        "128"
+      ],
+      "favorite_count" : "1",
+      "in_reply_to_status_id_str" : "1580398118557786112",
+      "id_str" : "1580399945810554880",
+      "in_reply_to_user_id" : "758598429115310080",
+      "truncated" : false,
+      "retweet_count" : "0",
+      "id" : "1580399945810554880",
+      "in_reply_to_status_id" : "1580398118557786112",
+      "created_at" : "Thu Oct 13 03:28:04 +0000 2022",
+      "favorited" : false,
+      "full_text" : "@cassiecodes No, I'm afraid I was the one who missed you at this conference. 🤔\n\nGuess we'll need to coordinate better next time.",
+      "lang" : "en",
+      "in_reply_to_screen_name" : "cassiecodes",
+      "in_reply_to_user_id_str" : "758598429115310080"
+    }
+  },
+  {
+    "tweet" : {
+      "retweeted" : false,
+      "source" : "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+      "entities" : {
+        "hashtags" : [ ],
+        "symbols" : [ ],
+        "user_mentions" : [
+          {
+            "name" : "Cassie Evans",
+            "screen_name" : "cassiecodes",
+            "indices" : [
+              "0",
+              "12"
+            ],
+            "id_str" : "758598429115310080",
+            "id" : "758598429115310080"
+          }
+        ],
+        "urls" : [ ]
+      },
+      "display_text_range" : [
+        "0",
+        "26"
+      ],
+      "favorite_count" : "1",
+      "in_reply_to_status_id_str" : "1580354743662424064",
+      "id_str" : "1580388620329811969",
+      "in_reply_to_user_id" : "758598429115310080",
+      "truncated" : false,
+      "retweet_count" : "0",
+      "id" : "1580388620329811969",
+      "in_reply_to_status_id" : "1580354743662424064",
+      "created_at" : "Thu Oct 13 02:43:03 +0000 2022",
+      "favorited" : false,
+      "full_text" : "@cassiecodes The Best Suit",
+      "lang" : "en",
+      "in_reply_to_screen_name" : "cassiecodes",
+      "in_reply_to_user_id_str" : "758598429115310080"
+    }
+  },
+  {
+    "tweet" : {
+      "retweeted" : false,
+      "source" : "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+      "entities" : {
+        "hashtags" : [ ],
+        "symbols" : [ ],
+        "user_mentions" : [
+          {
+            "name" : "CSS Evils 😈 !important",
+            "screen_name" : "cassiecodes",
+            "indices" : [
+              "0",
+              "12"
+            ],
+            "id_str" : "758598429115310080",
+            "id" : "758598429115310080"
+          }
+        ],
+        "urls" : [ ]
+      },
+      "display_text_range" : [
+        "0",
+        "27"
+      ],
+      "favorite_count" : "0",
+      "in_reply_to_status_id_str" : "1584846774505570304",
+      "id_str" : "1585093373178023937",
+      "in_reply_to_user_id" : "758598429115310080",
+      "truncated" : false,
+      "retweet_count" : "0",
+      "id" : "1585093373178023937",
+      "in_reply_to_status_id" : "1584846774505570304",
+      "created_at" : "Wed Oct 26 02:18:04 +0000 2022",
+      "favorited" : false,
+      "full_text" : "@cassiecodes yay, congrats!",
+      "lang" : "en",
+      "in_reply_to_screen_name" : "cassiecodes",
+      "in_reply_to_user_id_str" : "758598429115310080"
+    }
+  }
+]
